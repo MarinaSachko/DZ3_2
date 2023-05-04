17e1efb8a61cb15305150344936e1ce220d8821b
Marina.Sachko <marina.sachko@mail.ru>
2023-05-04 07:39:07 +0300

    correcting2 commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,7 @@
 public class BmiService {
     public int calculate(float weight, float height) {
-        float index = weight/((height/100)*(height/100));
+        float index = weight / ((height / 100) * (height / 100));
         int rezult = (int) index;
         return rezult;
     }
-
 }
